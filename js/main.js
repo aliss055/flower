fd2e0475efe982869df7cd6108afc2373c22cdea
aliss055 <harlequinfairy074@gmail.com>
2025-03-22 00:59:59 -0600

    agregue un cambio, con el creador de la pagina

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('Te amo muchote').split('')
+      const titles = ('De tus flores amarillas mailobe, si, son blancas pwro ekis nome importa').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
